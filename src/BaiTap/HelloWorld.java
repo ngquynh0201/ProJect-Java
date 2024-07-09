2b3c8b6eae2cbdda4e7e6eca0fc42de82c4a4957
ngquynh0201 <nguyenquynhpy2020@gmail.com>
2024-07-10 00:59:55 +0700

    init project

---
+++ src/BaiTap/HelloWorld.java
@@ -0,0 +1,10 @@
+package BaiTap;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World with java");
+		System.out.println("Hello World with java");
+		System.out.println("Hello World with java");
+	}
+
+}
